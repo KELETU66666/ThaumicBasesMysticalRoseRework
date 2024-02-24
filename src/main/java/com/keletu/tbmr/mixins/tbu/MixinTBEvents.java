9e93ccd8cea9b84e9953b1d08a6c287fcfdccefd
a <EYCHFSJD@gmail.com>
2024-02-24 13:58:01 +0800

    elder knowledge will give research point

---
+++ src/main/java/com/keletu/tbmr/mixins/tbu/MixinTBEvents.java
@@ -0,0 +1,65 @@
+package com.keletu.tbmr.mixins.tbu;
+
+import com.rumaruka.thaumicbases.common.handlers.EnchatmentHandler;
+import com.rumaruka.thaumicbases.init.TBEnchant;
+import com.wonginnovations.oldresearch.OldResearch;
+import com.wonginnovations.oldresearch.api.OldResearchApi;
+import com.wonginnovations.oldresearch.common.lib.network.PacketAspectPool;
+import com.wonginnovations.oldresearch.common.lib.network.PacketHandler;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.EntityList;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemSword;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import thaumcraft.api.aspects.AspectList;
+
+@Mixin(EnchatmentHandler.class)
+public class MixinTBEvents {
+
+    /**
+     * @author
+     * @reason
+     */
+    @SubscribeEvent
+    @Overwrite(remap = false)
+    public void onMobDeath(LivingDeathEvent e) {
+        if (e.getSource().getTrueSource() instanceof EntityPlayer) {
+            EntityPlayer attacker = (EntityPlayer)e.getSource().getTrueSource();
+            EntityLivingBase receiver = e.getEntityLiving();
+            ItemStack mainHund = attacker.getHeldItemMainhand();
+            if (!mainHund.isEmpty() && mainHund.getItem() instanceof ItemSword && EnchantmentHelper.getEnchantmentLevel(TBEnchant.elderKnowledge, mainHund) > 0) {
+                int enchLevel = EnchantmentHelper.getEnchantmentLevel(TBEnchant.elderKnowledge, mainHund);
+                if(attacker.world.rand.nextInt(Math.max(1, 7-enchLevel)) == 0)
+                {
+                    OldResearchApi.EntityTags eTags = null;
+                    for(int i = 0; i < OldResearchApi.scanEntities.size(); ++i)
+                    {
+                        OldResearchApi.EntityTags tags = OldResearchApi.scanEntities.get(i);
+                        if(tags != null && EntityList.getEntityString(receiver) != null && EntityList.getEntityString(receiver).equalsIgnoreCase(tags.entityName))
+                        {
+                            eTags = tags;
+                            break;
+                        }
+                    }
+                    if(eTags != null)
+                    {
+                        AspectList al = eTags.aspects;
+                        for(int i = 0; i < al.size(); ++i)
+                        {
+                            OldResearch.proxy.playerKnowledge.addAspectPool(attacker.getGameProfile().getName(), al.getAspects()[i], (short) 1);
+                            PacketHandler.INSTANCE.sendTo(new PacketAspectPool(al.getAspects()[i].getTag(), (short) 1, OldResearch.proxy.playerKnowledge.getAspectPoolFor(attacker.getGameProfile().getName(), al.getAspects()[i])), (EntityPlayerMP)attacker);
+                            if(attacker.world.rand.nextBoolean())
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
